0ae57b2f73d94d083e5e8d90139097366adf1d1a
LogicTechCorp <logictechcorp@gmail.com>
2015-05-03 22:24:26 -0700

    Adding village control

--- src/main/java/florasoma/util/BiomeBlockReplacer.java
+++ src/main/java/florasoma/util/BiomeBlockReplacer.java
@@ -3,6 +3,10 @@ package florasoma.util;
 import florasoma.FloraSoma;
 import florasoma.common.ConfigVillage;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockColored;
+import net.minecraft.block.BlockSlab;
+import net.minecraft.block.BlockStairs;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.BiomeDictionary.Type;
@@ -16,7 +20,6 @@ import java.util.Map;
 
 public class BiomeBlockReplacer implements IEventListener
 {
-
     private Map<Block, List<Pair<String, Block>>> replacements;
     private Map<Block, List<Pair<String, Integer>>> metadata;
 
@@ -30,22 +33,47 @@ public class BiomeBlockReplacer implements IEventListener
     @SubscribeEvent
     public void invoke(Event event)
     {
-        if(event instanceof BiomeEvent.GetVillageBlockID)
+        if (event instanceof BiomeEvent.GetVillageBlockID)
         {
             BiomeEvent.GetVillageBlockID ev = (BiomeEvent.GetVillageBlockID) event;
-            if(replacements.containsKey(ev.original.getBlock()))
+            if (replacements.containsKey(ev.original.getBlock()))
             {
-                for(Pair<String, Block> pair : replacements.get(ev.original.getBlock()))
+                for (Pair<String, Block> pair : replacements.get(ev.original.getBlock()))
                 {
-                    if (checkCondition(ev.biome, pair.left))
+                    if(metadata.containsKey(ev.original.getBlock()))
                     {
-                        ev.replacement = pair.right.getDefaultState();
-                        ev.setResult(Event.Result.DENY);
+                        for (Pair<String, Integer> pair1 : metadata.get(ev.original.getBlock()))
+                        {
+                            if (checkCondition(ev.biome, pair.left))
+                            {
+                                if (ev.original.getBlock() instanceof BlockStairs)
+                                {
+                                    ev.replacement = copyStairsState(ev.original, pair.right);
+                                } else if (ev.original.getBlock() instanceof BlockSlab)
+                                {
+                                    ev.replacement = copySlabState(ev.original, pair.right);
+                                } else
+                                {
+                                    if (checkCondition(ev.biome, pair1.left))
+                                    {
+                                        if (pair1.right == 0)
+                                        {
+                                            ev.replacement = pair.right.getDefaultState();
+                                        } else
+                                        {
+                                            ev.replacement = pair.right.getStateFromMeta(pair1.right);
+                                        }
+                                    }
+                                    }
+                                ev.setResult(Event.Result.DENY);
+                            }
+                        }
                     }
                 }
             }
         }
     }
+
 
     private static boolean checkCondition(BiomeGenBase biome, String condition)
     {
@@ -73,6 +101,20 @@ public class BiomeBlockReplacer implements IEventListener
             FloraSoma.instance.log.warn("Biome class: " + biome.getClass() + "; Condition: " + condition);
             return false;
         }
+    }
+
+    private static IBlockState copyStairsState(IBlockState original, Block replacement)
+    {
+        return replacement.getDefaultState()
+                .withProperty(BlockStairs.FACING, original.getValue(BlockStairs.FACING))
+                .withProperty(BlockStairs.HALF, original.getValue(BlockStairs.HALF))
+                .withProperty(BlockStairs.SHAPE, original.getValue(BlockStairs.SHAPE));
+    }
+
+    private static IBlockState copySlabState(IBlockState original, Block replacement)
+    {
+        return replacement.getDefaultState()
+                .withProperty(BlockSlab.HALF, original.getValue(BlockSlab.HALF));
     }
 }
 
